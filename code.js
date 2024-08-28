14886470d3b1e68e17fa46f7902ff8ab0c374ebc
CollinDavis03 <144744808+CollinDavis03@users.noreply.github.com>
2024-08-28 13:09:36 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
 function binarySearch(list, element) {
+    let left = 0;
+    let right = list.length -1; 
+    
+    while (left <= right) {
+        const middle = Math.floor((left + right) / 2); 
+
+        if (list[middle] === element) {
+            return middle; 
+        } else if (list[middle] < element) {
+            left = middle + 1; 
+        } else {
+            right = middle -1; 
+        }
+    }
     return -1;
 }
