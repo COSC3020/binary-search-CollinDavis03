7bd9b856bbfafd2070c6ea9f07f214cd7cd2a7ab
CollinDavis03 <144744808+CollinDavis03@users.noreply.github.com>
2024-08-28 20:12:20 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,12 @@
 function binarySearch(list, element) {
     let left = 0;
-    let right = list.length -1; 
+    let right = list.length - 1; 
     
     while (left <= right) {
+        // Calculate middle index
         const middle = Math.floor((left + right) / 2); 
 
+        // Compare middle element with the target element 
         if (list[middle] === element) {
             return middle; 
         } else if (list[middle] < element) {
